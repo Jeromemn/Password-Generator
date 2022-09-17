982b80e911dd498be9e390fc561fb1fb4156fc2f
nixon <jeromenixon95@gmail.com>
2022-09-16 21:59:32 -0700

    created loop for choosing password length, added array, added var to add characters

--- 03-Password-Generator/script.js
+++ 03-Password-Generator/script.js
@@ -14,25 +14,58 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Global variables 
-// const arryOfCharacters = ["lowercase", "uppercase", "numeric", "special characters"]
+const characterTypes = ["lowercase", "uppercase", "numeric", "special characters"]
 
 function generatePassword() {
   let length = prompt( "Choose length of password between 8 and 128 characters.");
-  if (length > 8 && length < 128) {
-    greeting = "Thank you";
-    console.log("length > 8 && < 128 Thank you!");
-  }else {
-    greeting = "Please choose between 8 and 128 characters";
-    let = length = prompt( "Choose length of password between 8 and 128 characters.");
-  if (length > 8 && length < 128) {
-    console.log("Please choose a length between 8 and 128 characters");
+  while (length <=8 || length >= 128) {
+    alert("Password must be between 8 and 128 characters. Try again!");
+   // let length = prompt( "Choose length of password between 8 and 128 characters.");
+    if (length >8 && length < 128) break;
   }
-  //let confirmSpecialCharacter= prompt("Select character type");
-  //if (confirmSpeciaCharacter = lowercase)
+    //console.log("length > 8 && < 128 Thank you!");
+  //}else {
+    
+    
+   // let = length = prompt( "Choose length of password between 8 and 128 characters.");
+ // if (length > 8 && length < 128) {
+   // console.log("Please choose a length between 8 and 128 characters");
+  //}
+  //let confirmLowercase = confirm("Do you want to use lowercase letters?");
+
+ // let confirmUppercase =confirm("Do you want to use uppercase letters?")
+
+ // let confirmUppercase =confirm("Do you want to use numbers")
+
+ // let confirmUppercase =confirm("Do you want to use cpecial characters?")
+  
+  //const confirmCharacterTypes = confirm(characterTypes[0]);
+   //  if (confirmAction) {
+    // if true
+  //  alert("Successfully added lowercase");
+      //}else {
+  //if false
+ //alert("Lowercase not added");
+//}
+  //let confirmCharacters = confirm( arryOfCharacters[0]);
+  //let confirmCharacters = confirm( arryOfCharacters[0]);
+  //let confirmCharacters = confirm( arryOfCharacters[0]);
+
+  const confirmCharactersTypes = [0];
+
+ for (let i = 0; i < characterTypes.length; i ++) {
+    const confirmed= confirm(`click okay if you want to include in ` + characterTypes[i] + ` characters`);
+    if (confirmed) {
+      alert(`Successfully added ` + characterTypes[i] + ` characters`)
+      //confirm(characterTypes.push (characterTypes[i])) 
+    }}}
+  // var textarea = document.
 
 
-  //else{
+  
+    
 
-  }
 
-}
+ // }
+
+//
