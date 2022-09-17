b02757f072ca98ab115b3785e1846ea1a586eaab
nixon <jeromenixon95@gmail.com>
2022-09-16 23:24:06 -0700

    fixed bugs in prompt and confirms

--- 03-Password-Generator/script.js
+++ 03-Password-Generator/script.js
@@ -20,8 +20,11 @@ function generatePassword() {
   let length = prompt( "Choose length of password between 8 and 128 characters.");
   while (length <=8 || length >= 128) {
     alert("Password must be between 8 and 128 characters. Try again!");
-   // let length = prompt( "Choose length of password between 8 and 128 characters.");
-    if (length >8 && length < 128) break;
+    length = prompt( "Choose length of password between 8 and 128 characters.");
+    if (length >8 && length < 128)  break;
+    if ("okay") {
+      console.log(length);
+    }
   }
     //console.log("length > 8 && < 128 Thank you!");
   //}else {
@@ -54,14 +57,35 @@ function generatePassword() {
   const confirmCharactersTypes = [0];
 
  for (let i = 0; i < characterTypes.length; i ++) {
-    const confirmed= confirm(`click okay if you want to include in ` + characterTypes[i] + ` characters`);
+    const confirmed= confirm(`click okay if you want to include  ` + characterTypes[i] + ` characters`);
     if (confirmed) {
       alert(`Successfully added ` + characterTypes[i] + ` characters`)
       //confirm(characterTypes.push (characterTypes[i])) 
-    }}}
+    }}
   // var textarea = document.
 
+// function makePassword(length) {
+ // var result = `length`;
+ // var lowercase = `abcdefghijklmnopqrstuvwxyz`;
+  //var uppercase = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+ // var numeric = `123456789`;
+  //var specialCharacters = `!#$%&*+-/<=>?@^_|~`;
 
+  //function generateString(length) 
+  // let result = ' ';
+  // const passwordLength = characters.length;
+
+  //setAttribute(textarea, )
+
+  //var passwordLength = prompt( "Choose length of password between 8 and 128 characters.");
+  //var lowercase = confirm ("Click okay if you want to include lowercase characters ")
+ // var uppercase = confirm ("Click okay if you want to include uppercase characters ")
+ // var numeric = confirm ("Click okay if you want to include numeric characters ")
+ // var specialCharacters = confirm ("Click okay if you want to include special characters ")
+
+
+
+}
   
     
 
